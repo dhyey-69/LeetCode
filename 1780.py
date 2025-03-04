a17639ac791eefd5e2e03e7258114b5203e5423f
Dhyey <dhyeybhatt66@gmail.com>
2025-03-04 09:41:50 +0530

    4th March

---
+++ 1780.py
@@ -0,0 +1,45 @@
+# 1780. Check if Number is a Sum of Powers of Three
+# Medium
+# Topics
+# Companies
+# Hint
+# Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+# An integer y is a power of three if there exists an integer x such that y == 3x.
+
+ 
+
+# Example 1:
+
+# Input: n = 12
+# Output: true
+# Explanation: 12 = 31 + 32
+# Example 2:
+
+# Input: n = 91
+# Output: true
+# Explanation: 91 = 30 + 32 + 34
+# Example 3:
+
+# Input: n = 21
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= n <= 107
+
+# num = 12
+num = 91
+# num = 21
+
+def xyz(n):
+    check = True
+
+    while (n > 0):
+        n, rem = divmod(n, 3)
+        if rem == 2:
+            check = False
+    return check
+        
+print(xyz(num))
