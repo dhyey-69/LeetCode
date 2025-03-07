da9b02b679ffd6ffa92f60b7710a2d63fdce6b3e
Dhyey <dhyeybhatt66@gmail.com>
2025-03-07 11:04:25 +0530

    7th March (2.0)

--- 2523.py
+++ 2523.py
@@ -37,10 +37,16 @@ r = 19
 # r = 6
 
 def isprime(n):
-    for i in range(2,n):
-        if n % i == 0:
+        if n < 2:
             return False
-    return True
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, int(n**0.5) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
 
 
 def xyz(left,right):
