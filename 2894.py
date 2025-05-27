a6095fd497bd42b69e89e933a068a71030debb28
Dhyey <dhyeybhatt66@gmail.com>
2025-05-27 09:00:30 +0530

    27th May (2.0)

--- 2894.py
+++ 2894.py
@@ -55,6 +55,16 @@ m = 3
 def xyz(n,m):
     num1 = sum(i for i in range(1, n + 1) if i % m != 0)
     num2 = sum(i for i in range(1, n + 1) if i % m == 0)
+
+    # num1 = 0
+    # num2 = 0
+
+    # for i in range(1,n+1):
+    #     if i % m == 0:
+    #         num2 += i
+    #     else:
+    #         num1 += i
+    
     return num1 - num2
 
 print(xyz(n, m))
