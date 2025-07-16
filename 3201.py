1536e816135bfc63cad2351828d4024c1e226fc8
Dhyey <dhyeybhatt66@gmail.com>
2025-07-16 08:54:12 +0530

    16th July 2025

---
+++ 3201.py
@@ -0,0 +1,74 @@
+# 3201. Find the Maximum Length of Valid Subsequence I
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given an integer array nums.
+# A subsequence sub of nums with length x is called valid if it satisfies:
+
+# (sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
+# Return the length of the longest valid subsequence of nums.
+
+# A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+
+# Output: 4
+
+# Explanation:
+
+# The longest valid subsequence is [1, 2, 3, 4].
+
+# Example 2:
+
+# Input: nums = [1,2,1,1,2,1,2]
+
+# Output: 6
+
+# Explanation:
+
+# The longest valid subsequence is [1, 2, 1, 2, 1, 2].
+
+# Example 3:
+
+# Input: nums = [1,3]
+
+# Output: 2
+
+# Explanation:
+
+# The longest valid subsequence is [1, 3].
+
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 2 * 105
+# 1 <= nums[i] <= 107
+
+
+n = [1,2,3,4]
+
+# n = [1,2,1,1,2,1,2]
+
+# n = [1,3]
+
+def xyz(nums):
+    n=len(nums)
+    if n==2: return 2
+    z=nums[0]&1
+    Len=[1-z, z, 1]
+    for xx in nums[1:]:
+        x=xx&1
+        Len[x&1]+=1
+        if x!=z:
+            Len[2]+=1
+            z=1-z
+    return max(Len)
+
+print(xyz(n))
