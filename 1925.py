61ed45109cb3b8ef6b05b03c1802e819ff1ed02b
Dhyey <dhyeybhatt66@gmail.com>
2025-12-08 09:24:15 +0530

    8th December 2025

---
+++ 1925.py
@@ -0,0 +1,43 @@
+# 1925. Count Square Sum Triples
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+# Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+# Example 1:
+
+# Input: n = 5
+# Output: 2
+# Explanation: The square triples are (3,4,5) and (4,3,5).
+# Example 2:
+
+# Input: n = 10
+# Output: 4
+# Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+
+# Constraints:
+
+# 1 <= n <= 250
+
+
+n = 5
+# n = 10
+
+def xyz(n):
+    ans = 0
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            c2 = a*a + b*b
+            c = int(c2**0.5)
+            if c*c == c2 and c <= n:
+                ans += 1
+    return ans
+
+print(xyz(n))
