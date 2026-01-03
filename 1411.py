6f9be1e69f3561509693001adabbc2812b298407
Dhyey <dhyeybhatt66@gmail.com>
2026-01-03 08:51:15 +0530

    3rd January 2026

---
+++ 1411.py
@@ -0,0 +1,45 @@
+# 1411. Number of Ways to Paint N × 3 Grid
+# Hard
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors: Red, Yellow, or Green while making sure that no two adjacent cells have the same color (i.e., no two cells that share vertical or horizontal sides have the same color).
+
+# Given n the number of rows of the grid, return the number of ways you can paint this grid. As the answer may grow large, the answer must be computed modulo 109 + 7.
+
+ 
+
+# Example 1:
+
+
+# Input: n = 1
+# Output: 12
+# Explanation: There are 12 possible way to paint the grid as shown.
+# Example 2:
+
+# Input: n = 5000
+# Output: 30228214
+ 
+
+# Constraints:
+
+# n == grid.length
+# 1 <= n <= 5000
+
+
+n = 1
+# n = 5000
+
+def xyz(n):
+    MOD = 10**9 + 7
+        
+    a = 6
+    b = 6
+    
+    for _ in range(2, n + 1):
+        a, b = (3*a + 2*b) % MOD, (2*a + 2*b) % MOD
+    
+    return (a + b) % MOD
+
+print(xyz(n))
